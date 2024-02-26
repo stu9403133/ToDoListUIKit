615528980165981dbef0b85b72b75aba8ca0a969
stu <stu382095@gmail.com>
2024-02-26 18:52:23 +0800

    transmit data by delegate but there is some bugs

--- ToDoListUIKit/Edit/AddOrEditTableViewController.swift
+++ ToDoListUIKit/Edit/AddOrEditTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class AddOrEditTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    weak var delegate: AddOrEditTableViewControllerDelegate?
+    
     var isSelectedImage = false
     
     var editList: ToDoList!
@@ -50,7 +52,6 @@ class AddOrEditTableViewController: UITableViewController, UIImagePickerControll
         }
     }
     
-    
     func updateButton(priority: Priority) {
         prioirtyCircle.tintColor = UIColor(named: "\(editList.priority.rawValue )")
         urgentButton.backgroundColor = .white
@@ -81,7 +82,6 @@ class AddOrEditTableViewController: UITableViewController, UIImagePickerControll
         editList.priority = .urgent
         updateButton(priority: editList.priority)
     }
-    
   
     @IBAction func showPic(_ sender: Any) {
         let picAlertController = UIAlertController(title: "照片來源", message: "請選擇", preferredStyle: .actionSheet)
@@ -105,9 +105,6 @@ class AddOrEditTableViewController: UITableViewController, UIImagePickerControll
         present(picAlertController, animated: true)
     }
     
-    
-    
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             isSelectedImage = true
@@ -126,21 +123,7 @@ class AddOrEditTableViewController: UITableViewController, UIImagePickerControll
         view.endEditing(true)
     }
     
- // 頁面跳轉與傳資料
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        editList.title = titleLabel.text!
-        if editList.title.isEmpty == false {
-            return true
-        } else {
-            let alertController = UIAlertController(title: "請填入標題內容喔", message: "0.0", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default)
-            alertController.addAction(okAction)
-            present(alertController, animated: true)
-            return false
-        }
-    }
-        
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    @IBAction func saveButton(_ sender: Any) {
         var fileName: String?
         let title = titleLabel.text ?? ""
         let priority = editList.priority
@@ -155,22 +138,47 @@ class AddOrEditTableViewController: UITableViewController, UIImagePickerControll
             let data = listImage.image?.jpegData(compressionQuality: 1)
             let url = URL.homeDirectory.appending(path: fileName!).appendingPathExtension("jpeg")
             try? data?.write(to: url)
-            
+
         } else {
             if let file = editList.imageFileName {
                 fileName = file
             }
         }
         editList = ToDoList(title: title, priority: priority, detail: detail, imageFileName: fileName)
+        
+        if editList.title.isEmpty == false {
+            delegate?.addOrEditTableViewController(self, didlist: editList)
+            performSegue(withIdentifier: "unwindBySave", sender: nil)
+        } else {
+            let alertController = UIAlertController(title: "請填入標題內容喔", message: "0.0", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            alertController.addAction(okAction)
+            present(alertController, animated: true)
+        }
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
+        
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        var fileName: String?
+       
+//        if isSelectedImage {
+//            isSelectedImage.toggle()
+//            if let file = editList.imageFileName {
+//                fileName = file
+//            } else {
+//                fileName = UUID().uuidString
+//            }
+//            let data = listImage.image?.jpegData(compressionQuality: 1)
+//            let url = URL.homeDirectory.appending(path: fileName!).appendingPathExtension("jpeg")
+//            try? data?.write(to: url)
+//
+//        } else {
+//            if let file = editList.imageFileName {
+//                fileName = file
+//            }
+//        }
+//        editList = ToDoList(title: title, priority: priority, detail: detail, imageFileName: fileName)
+//    }
     
 // 顯示清單數目
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -182,61 +190,9 @@ class AddOrEditTableViewController: UITableViewController, UIImagePickerControll
         // #warning Incomplete implementation, return the number of rows
         return 4
     }
-    /*
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-     
-     // Configure the cell...
-     
-     return cell
-     }
-     */
     
-    
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+}
+
+protocol AddOrEditTableViewControllerDelegate: AnyObject {
+    func addOrEditTableViewController(_ controller: AddOrEditTableViewController, didlist editList: ToDoList)
 }
--- ToDoListUIKit/ToDoList/ToDoListTableViewController.swift
+++ ToDoListUIKit/ToDoList/ToDoListTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ToDoListTableViewController: UITableViewController {
-
+    var list: ToDoList!
     var lists = [ToDoList]() {
         didSet{
             ToDoList.saveLists(lists)
@@ -24,7 +24,7 @@ class ToDoListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     func updateData() {
-        UserDefaults.standard.removeObject(forKey: "lists")
+//        UserDefaults.standard.removeObject(forKey: "lists")
         if let lists = ToDoList.readLists() {
             self.lists = lists
             print("updateData(): 以解碼", lists)
@@ -70,80 +70,39 @@ class ToDoListTableViewController: UITableViewController {
         if let row = tableView.indexPathForSelectedRow?.row {
             controller?.editList = lists[row]
         }
+        controller?.delegate = self
         return controller
     }
     
 
     
     @IBAction func unwindToList(_ unwindSegue: UIStoryboardSegue) {
-        if let source = unwindSegue.source as? AddOrEditTableViewController,
-           let list = source.editList {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                lists[indexPath.row] = list
-                print("ListTableView", lists)
-                tableView.reloadRows(at: [indexPath], with: .automatic)
-            } else {
-                lists.insert(list, at: 0)
-                let newIndexPath = IndexPath(row: 0, section: 0)
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
-            }
+        tableView.reloadData()
+        print("unwindToList", lists)
+            
         }
-    }
 
     
     // 刪除表單
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        
-//        try? FileManager.default.removeItem(at: lists[indexPath.row].photoURL!)
         lists.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .automatic)
         tableView.reloadData()
         print("after deleted", lists)
     }
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+}
+
+extension ToDoListTableViewController: AddOrEditTableViewControllerDelegate{
+    func addOrEditTableViewController(_ controller: AddOrEditTableViewController, didlist editList: ToDoList) {
+        print("protocol use")
+        list = editList
+        if let indexPath = tableView.indexPathForSelectedRow {
+            lists[indexPath.row] = list
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        } else {
+            lists.insert(list, at: 0)
+            let newIndexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
